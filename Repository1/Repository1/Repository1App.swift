2061f78b08b0ed742bda28675a4507e5a53659a2
Andrei <101584932+DashiellVienna@users.noreply.github.com>
2024-12-16 15:38:16 +0100

    test

---
+++ Repository1/Repository1/Repository1App.swift
@@ -0,0 +1,17 @@
+//
+//  Repository1App.swift
+//  Repository1
+//
+//  Created by Andrei Deshin on 16.12.24.
+//
+
+import SwiftUI
+
+@main
+struct Repository1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
